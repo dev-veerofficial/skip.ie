944c516494028dac601b1734b9dccd7c6e262b6b
Vinay Singh <vishacademy@gmail.com>
2020-06-17 18:13:40 +0530

    faq arrow issue solved

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ $(document).on('click', '#accordion a', function () {
     let trigger = $(this);
     let icon = trigger.find('i');
 
-    trigger.parents('.card').find('a i.rotate').removeClass('rotate');
+    trigger.parents('.card').siblings('.card').find('a i.rotate').removeClass('rotate');
 
     if ($(this).children('i').hasClass('rotate')) {
         icon.removeClass('rotate');
-        alert(1);
+        // alert(1);
     } else {
         icon.addClass('rotate');
     }
