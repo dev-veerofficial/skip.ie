53874aec7b88e6c2a8b840f5d8c81996fb4854df
Vinay Singh <vishacademy@gmail.com>
2020-06-17 18:11:22 +0530

    customization

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).on('click', '#accordion a', function () {
+    let trigger = $(this);
+    let icon = trigger.find('i');
+
+    trigger.parents('.card').find('a i.rotate').removeClass('rotate');
+
+    if ($(this).children('i').hasClass('rotate')) {
+        icon.removeClass('rotate');
+        alert(1);
+    } else {
+        icon.addClass('rotate');
+    }
+})
